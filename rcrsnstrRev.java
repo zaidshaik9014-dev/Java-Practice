d0fe5ac471cc4e38b796430b00909ffd2f03d071
zaidshaik9014-dev <zaidshaik9014@gamil.com>
2026-01-31 11:14:10 +0530

    Added a new Program

---
+++ rcrsnstrRev.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class rcrsnstrRev {
+    public static void strRev(String str, int idx) {
+        if(idx==0) {
+            System.out.println(str.charAt(idx));
+            return;
+        }
+        System.out.print(str.charAt(idx));
+        strRev(str, idx-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        strRev(str, str.length()-1);
+    }
+}
